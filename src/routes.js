1420343ca3e8707b868d991560fc9268d68330d6
Emanuel Marcos dos Santos Barboza <emanuel@tigrara.local>
2023-05-16 17:18:34 -0300

    Adicionando arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import Menu from "componentes/Menu";
+import PaginaPadrao from "componentes/PaginaPadrao";
+import Rodape from "componentes/Rodape";
 import Inicio from "paginas/Inicio";
 import SobreMim from "paginas/SobreMim";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -12,10 +14,14 @@ function AppRoutes() {
       <Menu />
 
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobremim" element={<SobreMim />} />
+        </Route>
         <Route path="*" element={<div>Página não encontrada</div>} />
       </Routes>
+ 
+      <Rodape />
     </BrowserRouter>
   );
 }
