aeb26e486cf6258a87b06346711c732b11d55481
Emanuel Marcos dos Santos Barboza <emanuel@tigrara.local>
2023-05-16 11:28:30 -0300

    Adicionando banner e terminando navegação

---
+++ src/paginas/Inicio/index.js
@@ -0,0 +1,11 @@
+import Banner from 'componentes/Banner'
+import React from 'react'
+
+export default function Inicio() {
+  return (
+    <main>
+      <Banner />
+      <h1>Olá Mundo!</h1>
+    </main>
+  )
+}
---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import Menu from "componentes/Menu";
+import Inicio from "paginas/Inicio";
+import SobreMim from "paginas/SobreMim";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// console.log(window.location);
+// const pagina = window.location.pathname === '/' ? <Inicio /> : <SobreMim />
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Menu />
+
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
